22352c3d75b4ac44432f215594e181e9d34d92ac
Paulos Gebeyehu <paulos@shega.org>
2024-10-23 14:55:05 +0300

    Update plugin name for strapi and removed from being displayed on admin dashboard menu

---
+++ Excerpt-Generator-Project/excerpt-generator-project/config/plugins.js
@@ -0,0 +1,6 @@
+module.exports = () => ({
+    'excerpt-and-seo-generator':{
+      enabled:true, 
+      resolve: "./src/plugins/excerpt-and-seo-generator"
+    },
+});
---
+++ Excerpt-Generator-Project/excerpt-generator-project/types/generated/components.d.ts
@@ -0,0 +1,40 @@
+import type { Schema, Attribute } from '@strapi/strapi';
+
+export interface SeoSeo extends Schema.Component {
+  collectionName: 'components_seo_seos';
+  info: {
+    displayName: 'seo';
+    description: '';
+  };
+  attributes: {
+    metaTitle: Attribute.String;
+    metaDescription: Attribute.Text;
+  };
+}
+
+export interface ContentContent extends Schema.Component {
+  collectionName: 'components_content_contents';
+  info: {
+    displayName: 'body';
+    description: '';
+  };
+  attributes: {
+    body: Attribute.RichText &
+      Attribute.CustomField<
+        'plugin::ckeditor.CKEditor',
+        {
+          output: 'HTML';
+          preset: 'rich';
+        }
+      >;
+  };
+}
+
+declare module '@strapi/types' {
+  export module Shared {
+    export interface Components {
+      'seo.seo': SeoSeo;
+      'content.content': ContentContent;
+    }
+  }
+}
