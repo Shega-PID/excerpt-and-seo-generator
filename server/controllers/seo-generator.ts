594a32a45909b9b48d8f01aab844776816ea6af3
pos1234 <paulosegebeyehu5@gmail.com>
2024-11-14 13:26:45 +0300

    Updated version

--- server/controllers/seo-generator.ts
+++ server/controllers/seo-generator.ts
@@ -6,7 +6,7 @@ export default ({ strapi }: { strapi: Strapi }) => ({
         const { title, content } = ctx.request.body
         const titleData = title ? `title = ${title} and ` : ''
         const selectPrompt =  process.env.GENERATE_SEO_PROMPT ? `${process.env.GENERATE_SEO_PROMPT} from ${content} and ${titleData} return only in JSON format` : `Generate an SEO metadata description maximum 160 character's long from ${content} and extract keywords from ${titleData}; ensure the description does not exceed the character limit; return only in JSON format.`
-
+  
         const generateResponse = async () => {
             const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GENERATE_AI_API_KEY as string);
             const model = genAI.getGenerativeModel({ model: "gemini-1.0-pro" });
