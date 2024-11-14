362a5a8b0a4c8b7205508b5042a0acad6004c5d7
pos1234 <paulosegebeyehu5@gmail.com>
2024-11-14 13:16:44 +0300

    updated version

--- server/controllers/excerpt-generator.ts
+++ server/controllers/excerpt-generator.ts
@@ -6,7 +6,7 @@ export default ({ strapi }: { strapi: Strapi }) => ({
         const { content } = ctx.request.body
         const selectPrompt =  process.env.GENERATE_EXCERPT_PROMPT ? `${process.env.GENERATE_EXCERPT_PROMPT} from ${content}` : `Write a 200 character excerpt from ${content} and remove the title in response.`
         const generateResponse = async () => {
-            const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GENERATE_AI_API_KEY as string);
+            const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GENERATE_AI_API_KEY as string); 
             const model = genAI.getGenerativeModel({ model: "gemini-1.0-pro" });
             const result = await model.generateContent(selectPrompt);
             return result
