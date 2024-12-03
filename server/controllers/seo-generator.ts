3a634a0ea2e3197acc2e8eb1f231b8e7aaa2e785
pos1234 <paulosegebeyehu5@gmail.com>
2024-12-03 11:16:20 +0300

    Refactored and changed the AI to be Groq

--- server/controllers/seo-generator.ts
+++ server/controllers/seo-generator.ts
@@ -6,7 +6,7 @@ export default ({ strapi }: { strapi: Strapi }) => ({
     const titleData = title ? `title = ${title} and ` : "";
     const selectPrompt = process.env.GENERATE_SEO_PROMPT
       ? `${process.env.GENERATE_SEO_PROMPT} from ${content} and ${titleData} return only in JSON format`
-      : `Generate an SEO metadata description maximum 160 character's long from ${content} and extract keywords from ${titleData}; ensure the description does not exceed the character limit; return all as one JSON format with attribute names description and keywords.`;
+      : `Generate an SEO metadata description maximum 160 character's long from ${content} and extract keywords from ${titleData}; ensure the description does not exceed the character limit; return all as one JSON format with attribute names description and keywords. `;
     const result = await generateResponse(selectPrompt);
     const response = {
       result: result,
